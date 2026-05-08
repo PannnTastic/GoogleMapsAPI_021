b288136165e28abb6795a5d5873648368a41537d
PannnTastic <pannntastic@gmail.com>
2026-05-08 10:57:50 +0700

    Add address selection functionality to MapPage widget

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -2,11 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget{
-  const MapPage({super.key})
+  const MapPage({super.key});
 
   @override
   State<MapPage> createState() => _MapPageState();
@@ -37,7 +38,7 @@ class _MapPageState extends State<MapPage> {
         zoom: 16,
       );
 
-      final placemarks = await.placemarkFromCoordinates(
+      final placemarks = await placemarkFromCoordinates(
         _currentPosition!.latitude,
         _currentPosition!.longitude
       );
@@ -74,4 +75,70 @@ class _MapPageState extends State<MapPage> {
     return Geolocator.getCurrentPosition();
   }
 
+  Future<void> _onTap(LatLng latlng) async{
+    final placemarks = await placemarkFromCoordinates(
+      latlng.latitude,
+      latlng.longitude
+    );
+    final p = placemarks.first;
+    setState(() {
+      _pickedMarker = Marker(
+        markerId: const MarkerId("picked"),
+        position: latlng,
+        infoWindow: InfoWindow(
+          title: p.name?.isNotEmpty == true ? p.name : "Lokasi Dipilih",
+          snippet: "${p.street}, ${p.locality}",
+        )
+      );
+    });
+    final ctrl = await _ctrl.future;
+    await ctrl.animateCamera(CameraUpdate.newLatLngZoom(latlng, 16));
+
+    setState(() {
+      _pickedAddress = "${p.name}, ${p.street}, ${p.locality}, ${p.country}, ${p.postalCode}";
+    });
+  }
+
+  void _confirmSelection(){
+    showDialog(
+        context: context,
+        builder:
+        (_) => AlertDialog(
+          title: const Text("Konfirmasi Alamat"),
+          content: Text(_pickedAddress ?? ""),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Batal"),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pop(context);
+                Navigator.pop(context,_pickedAddress);
+              },
+              child: const Text("Pilih"),
+            )
+          ],
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(_initialcamera == null){
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pilih Alamat"),
+      ),
+      body: SafeArea(
+
+      ),
+    );
+  }
 }
