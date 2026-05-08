c64df8368ca81a3cf28cac4cd7bb3d6ffb81ac73
PannnTastic <pannntastic@gmail.com>
2026-05-08 10:05:33 +0700

    Add HomePage widget to home_page.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState()=> _HomePageState();
+}
+
